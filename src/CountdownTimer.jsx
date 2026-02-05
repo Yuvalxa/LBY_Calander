cedfd4c97c1e8e74ab34528f20fabfa9ca621a01
Yuval Mastey <yuval.mastey@example.com>
2026-02-05 10:42:04 +0200

    Fix image loading for GitHub Pages by importing images as modules

--- src/CountdownTimer.jsx
+++ src/CountdownTimer.jsx
@@ -3,6 +3,68 @@ import quotesData from './quotes.json';
 import picturesData from './pictures.json';
 import './CountdownTimer.css';
 
+// Import all day images so Vite processes them
+import day01 from './images/day-01.jpg';
+import day02 from './images/day-02.svg';
+import day03 from './images/day-03.svg';
+import day04 from './images/day-04.svg';
+import day05 from './images/day-05.svg';
+import day06 from './images/day-06.svg';
+import day07 from './images/day-07.svg';
+import day08 from './images/day-08.svg';
+import day09 from './images/day-09.svg';
+import day10 from './images/day-10.svg';
+import day11 from './images/day-11.svg';
+import day12 from './images/day-12.svg';
+import day13 from './images/day-13.svg';
+import day14 from './images/day-14.svg';
+import day15 from './images/day-15.svg';
+import day16 from './images/day-16.svg';
+import day17 from './images/day-17.svg';
+import day18 from './images/day-18.svg';
+import day19 from './images/day-19.svg';
+import day20 from './images/day-20.svg';
+import day21 from './images/day-21.svg';
+import day22 from './images/day-22.svg';
+import day23 from './images/day-23.svg';
+import day24 from './images/day-24.svg';
+import day25 from './images/day-25.svg';
+import day26 from './images/day-26.svg';
+import day27 from './images/day-27.svg';
+import day28 from './images/day-28.svg';
+
+// Map day numbers to imported images
+const imageMap = {
+  1: day01,
+  2: day02,
+  3: day03,
+  4: day04,
+  5: day05,
+  6: day06,
+  7: day07,
+  8: day08,
+  9: day09,
+  10: day10,
+  11: day11,
+  12: day12,
+  13: day13,
+  14: day14,
+  15: day15,
+  16: day16,
+  17: day17,
+  18: day18,
+  19: day19,
+  20: day20,
+  21: day21,
+  22: day22,
+  23: day23,
+  24: day24,
+  25: day25,
+  26: day26,
+  27: day27,
+  28: day28,
+};
+
 export default function CountdownTimer() {
   const [timeLeft, setTimeLeft] = useState({
     days: 0,
@@ -39,20 +101,21 @@ export default function CountdownTimer() {
         }
 
         // Get picture for current day
+        if (imageMap[daysFromStart]) {
+          setCurrentPicture(imageMap[daysFromStart]);
+        }
         const picture = picturesData.pictures.find((p) => p.day === daysFromStart);
         if (picture) {
-          setCurrentPicture(picture.image);
           setPictureCaption(picture.caption);
         }
       } else {
         setTimeLeft({ days: 0, hours: 0, minutes: 0, seconds: 0 });
         setCurrentQuote("🎉 LBY HAS LEFT! 🎉 The legend has departed!");
         setQuoteType('final');
-        const lastPicture = picturesData.pictures.find((p) => p.day === 28);
-        if (lastPicture) {
-          setCurrentPicture(lastPicture.image);
-          setPictureCaption("The Legend Has Departed!");
+        if (imageMap[28]) {
+          setCurrentPicture(imageMap[28]);
         }
+        setPictureCaption("The Legend Has Departed!");
       }
     };
 
